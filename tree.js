04b8c69bbd0832f04b0fb6d6a1af3cb7583a7efe
dariuszstawik <dariusz.stawik@gmail.com>
2019-01-25 10:53:58 +0100

    Init

---
+++ tree.js
@@ -0,0 +1,15 @@
+
+star="*";
+emptySpace="          ";
+
+for (let i=0; i<10; i++) {
+    console.log(emptySpace,star);
+    emptySpace=emptySpace.replace(" ", "");
+    star=star+"**";
+
+}
+
+    console.log("           @");
+    console.log("          ###");
+    console.log("         &&&&&");
+    
