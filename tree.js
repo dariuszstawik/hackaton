1359bcc5062b83fe02180286e0925585ece32d80
dariuszstawik <dariusz.stawik@gmail.com>
2019-01-28 00:31:38 +0100

    some improvments in the code after hackaton

--- tree.js
+++ tree.js
@@ -1,15 +1,49 @@
+// WERSJA KODU Z HACKATONU
 
-star="*";
-emptySpace="          ";
+// star="*";
+// emptySpace="          ";
 
-for (let i=0; i<10; i++) {
-    console.log(emptySpace,star);
-    emptySpace=emptySpace.replace(" ", "");
-    star=star+"**";
+// for (let i=0; i<10; i++) {
+//     console.log(emptySpace,star);
+//     emptySpace=emptySpace.replace(" ", "");
+//     star=star+"**";
 
+// }
+
+//     console.log("           @");
+//     console.log("          ###");
+//     console.log("         &&&&&");
+
+
+// NOWA WERSJA KODU
+
+let h=25;
+let star="*";
+let emptySpace="";
+
+const emptySpacesAmount = function(height) {
+    let space=" ";
+    for (i=0; i<h; i++) {
+        emptySpace+=space;
+    }
 }
 
-    console.log("           @");
-    console.log("          ###");
-    console.log("         &&&&&");
-    
+const treeHead = function() {
+    let emptySpaceHead = emptySpace;
+
+    for (let i=0; i<h; i++) {
+        console.log(emptySpaceHead,star);
+        emptySpaceHead=emptySpaceHead.replace(" ", "");
+        star=star+"**";
+    }
+}
+
+const treeBottom = function() {
+    console.log(emptySpace+"@@");
+    console.log(emptySpace+"##");
+    console.log(emptySpace+"&&");
+}
+
+emptySpacesAmount(h);
+treeHead();
+treeBottom();
